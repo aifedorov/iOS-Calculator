73a8a9d5af478746d51742dbef0ef2a329dcebc2
alexander <fairk19@gmail.com>
2015-11-18 20:40:30 +0300

    Создание нового проекта

---
+++ Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Александр on 18.11.15.
+//  Copyright © 2015 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
